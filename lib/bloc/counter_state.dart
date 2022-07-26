cf9d888c4a465376ab2a0ef3d5f55999bb09f3e1
EDR3X <dhunganaannanta@gmail.com>
2022-07-26 16:58:53 +0545

    del

--- lib/bloc/counter_state.dart
+++ lib/bloc/counter_state.dart
@@ -2,6 +2,7 @@ part of 'counter_bloc.dart';
 
 class CounterState extends Equatable {
   final int counter;
+
   const CounterState({
     required this.counter,
   });
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:event_transformer/another.dart';
 import 'package:event_transformer/bloc/counter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,6 +14,7 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const Navitation(),
             Text(
               "${context.watch<CounterBloc>().state.counter}",
               style: const TextStyle(fontSize: 34),
@@ -40,6 +42,27 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class Navitation extends StatelessWidget {
+  const Navitation({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: const Text('data'),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const AnotherPage(),
+          ),
+        );
+      },
     );
   }
 }
